c0e71d758af61d89367a6298023a10cf164febd4
ThatFunnyWeirdo <112593486+ThatFunnyWeirdo@users.noreply.github.com>
2023-08-07 15:56:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,118 @@
+image = document.getElementById("myImage");
+
+function changeImage()
+{
+    image.src = "images/small_axolotl_back.jpeg";
+}
+
+function changeImageBack()
+{
+    image.src = "images/small_axolotl_front.jpeg";
+}
+
+image2 = document.getElementById("myImage2");
+
+function changeImage2()
+{
+    image2.src = "images/axolotl_back.jpeg";
+}
+
+function changeImageBack2()
+{
+    image2.src = "images/axolotl_front.jpeg";
+}
+
+image3 = document.getElementById("myImage3");
+
+function changeImage3()
+{
+    image3.src = "images/turtle_back.jpeg";
+}
+
+function changeImageBack3()
+{
+    image3.src = "images/turtle_front.jpeg";
+}
+
+image4 = document.getElementById("myImage4");
+
+function changeImage4()
+{
+    image4.src = "images/whale_back.jpeg";
+}
+
+function changeImageBack4()
+{
+    image4.src = "images/whale_front.jpeg";
+}
+
+image5 = document.getElementById("myImage5");
+
+function changeImage5()
+{
+    image5.src = "images/pearbear_back.jpeg";
+}
+
+function changeImageBack5()
+{
+    image5.src = "images/pearbear_front.jpeg";
+}
+
+image6 = document.getElementById("myImage6");
+
+function changeImage6()
+{
+    image6.src = "images/octopus_back.jpeg";
+}
+
+function changeImageBack6()
+{
+    image6.src = "images/octopus_front.jpeg";
+}
+
+image7 = document.getElementById("myImage7");
+
+function changeImage7()
+{
+    image7.src = "images/miny_octopus_back.jpeg";
+}
+
+function changeImageBack7()
+{
+    image7.src = "images/miny_octopus_front.jpeg";
+}
+
+function changeLocation()
+{
+    window.location = "small_axolotl.html";
+}
+
+function changeLocation2()
+{
+    window.location = "axolotl.html";
+}
+
+function changeLocation3()
+{
+    window.location = "turtle.html";
+}
+
+function changeLocation4()
+{
+    window.location = "whale.html";
+}
+
+function changeLocation5()
+{
+    window.location = "pearbear.html";
+}
+
+function changeLocation6()
+{
+    window.location = "octopus.html";
+}
+
+function changeLocation7()
+{
+    window.location = "small_octopus.html";
+}
---
+++ small_octopus.js
@@ -0,0 +1,4 @@
+function changeLocation()
+{
+    window.location = "index.html";
+}
